5ee38a81779c923d60d4abdb6d8c58badc09ae10
Andrew Button <button-@users.noreply.github.com>
2014-11-20 12:47:37 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,5 @@
 # Bootstrap file to setup Puppet
 
-rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
-yum -y install puppet
+#rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+#yum -y install puppet
 touch /tmp/done
