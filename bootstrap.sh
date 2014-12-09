91c06bcc98140a4e7eae117c13d3eb42446ea0e7
Andrew Button <button-@users.noreply.github.com>
2014-12-09 16:43:24 +0000

    Added sudo for yum install

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ CENTOS_RELEASE=`rpm -q --queryformat "%{VERSION}" centos-release`
 if [ $CENTOS_RELEASE = "6" ] || [ $CENTOS_RELEASE = "7" ] ; then
   echo "$CENTOS_RELEASE"
   sudo rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-$CENTOS_RELEASE.noarch.rpm
-  yum -y install puppet
+  sudo yum -y install puppet
 else
   echo "Could not detect version of CentOS: $CENTOS_RELEASE"
   exit 1
